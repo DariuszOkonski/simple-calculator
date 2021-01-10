1946fae011590255945f2295a408ee6323b2533e
Dariusz Okonski <darek200180@gmail.com>
2021-01-10 17:55:30 +0000

    add display_frame for entry

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ display_font = ('Arial', 30)
 
 #define functions
 
+#gui layout
+#define farmes
+display_frame = tkinter.LabelFrame(root)
+button_frame = tkinter.LabelFrame(root)
+display_frame.pack()
+button_frame.pack()
+
+#layout for the display frame
+display = tkinter.Entry(display_frame, width=50, font=display_font, bg=white_green, borderwidth=5)
+display.pack(padx=5, pady=5)
+
+
+
 
 
 root.mainloop()
