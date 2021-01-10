a00f341fc9982dd47e781e643b7cc935beec2919
Dariusz Okonski <darek200180@gmail.com>
2021-01-10 17:50:32 +0000

    add basic colors and fonts

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello world")
+import tkinter
+
+#define window
+root = tkinter.Tk()
+root.title('Calculator')
+root.iconbitmap('calc.ico')
+root.geometry('300x400')
+root.resizable(0, 0)
+
+#define colors and fonts
+dark_green = '#93af22'
+light_green = '#acc253'
+white_green = '#edefe0'
+button_font = ('Arial', 18)
+display_font = ('Arial', 30)
+
+#define functions
+
+
+
+root.mainloop()
